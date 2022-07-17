d878eea1720cc93041bbdc92c05b576467824b9a
Grae <38518120+graemeniedermayer@users.noreply.github.com>
2022-07-16 22:26:39 -0600

    Update webxr-component.js

--- src/postProcessDOF/webxr-component.js
+++ src/postProcessDOF/webxr-component.js
@@ -1,5 +1,5 @@
 import {entity} from "../entity.js";
-import {THREE, AfterimagePass, RenderPass, EffectComposer, UnrealBloomPass, BokehPass} from '../post/three-defs.js';
+import {THREE, RenderPass, EffectComposer, BokehPass} from '../post/three-defs.js';
 // This is a little messy. I need to review JS context/scope
 
 export const xr_component = (() => {
